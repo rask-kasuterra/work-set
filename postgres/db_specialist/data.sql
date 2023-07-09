a3ceeb251b5915fa935d053c2ba62f0d7dacc894
Rask Kasuterra <kasuterra.524@gmail.com>
2023-07-09 21:55:01 +0900

    for reiwa 3rd year db specialist

---
+++ postgres/db_specialist/data.sql
@@ -0,0 +1,17 @@
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('1','oo線','Y','Y');
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('2','oo線','Y','N');
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('3','oo線','N','Y');
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('4','oo線','N','N');
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('5','xx線','Y','Y');
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('6','xx線','Y','N');
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('7','xx線','N','Y');
+insert into 物件 (物件コード,沿線,エアコン,オートロック) values ('8','xx線','N','N');
+
+insert into 新物件 (物件コード,物件名,沿線) values ('1','b1','oo線');
+insert into 新物件 (物件コード,物件名,沿線) values ('2','b2','oo線');
+insert into 新物件 (物件コード,物件名,沿線) values ('3','b3','oo線');
+insert into 新物件 (物件コード,物件名,沿線) values ('4','b4','oo線');
+insert into 新物件 (物件コード,物件名,沿線) values ('5','b5','xx線');
+insert into 新物件 (物件コード,物件名,沿線) values ('6','b6','xx線');
+insert into 新物件 (物件コード,物件名,沿線) values ('7','b7','xx線');
+insert into 新物件 (物件コード,物件名,沿線) values ('8','b8','xx線');
